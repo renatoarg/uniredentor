dc14f65147d53306c50be5c12c6478fc043c77b4
Renato Goncalves <renatoarg@gmail.com>
2020-06-10 13:24:00 -0300

    Integrando Jetpack Navigation

--- app/src/main/java/edu/renatoarg/uniredentor/ui/inputgrade/InputGradeFragment.kt
+++ app/src/main/java/edu/renatoarg/uniredentor/ui/inputgrade/InputGradeFragment.kt
@@ -3,7 +3,9 @@ package edu.renatoarg.uniredentor.ui.inputgrade
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import edu.renatoarg.uniredentor.R
+import kotlinx.android.synthetic.main.fragment_input_grade.*
 
 /**
  * A simple [Fragment] subclass.
@@ -11,6 +13,13 @@ import edu.renatoarg.uniredentor.R
 class InputGradeFragment : Fragment(R.layout.fragment_input_grade) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        submit.setOnClickListener {
+            when(gradle.text.toString().toInt()) {
+                in 7..Int.MAX_VALUE -> findNavController().navigate(R.id.action_inputGradeFragment_to_approvedFragment)
+                else -> findNavController().navigate(R.id.action_inputGradeFragment_to_failFragment)
+            }
+        }
     }
 
 }
